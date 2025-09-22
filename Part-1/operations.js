bf9362e32d246c028a8c4338a8043cb7a96d6ea3
subhadip07 <subhadipbag18@outlook.com>
2025-09-22 21:35:20 +0530

    add: operations.js

---
+++ Part-1/operations.js
@@ -0,0 +1,44 @@
+let score = 250;
+let bonus = 100;
+
+let total = score + bonus;
+console.log(total);
+
+let addition = 4 + 5;
+console.log(addition);
+
+let subtract = 9 - 3;
+console.log(subtract);
+
+let mult = 5 * 8;
+console.log(mult);
+
+let div = 8 / 2;
+console.log(div);
+
+let reminder = 9 % 2;
+console.log(reminder);
+
+let expo = 2 ** 3;
+console.log(expo);
+
+
+let myscore = 118;
+myscore++;
+console.log(myscore);
+
+
+let credits = 56;
+credits--;
+console.log(credits);
+
+// comparison operations
+let n1 = 3;
+let n2 = 3;
+let n3 = 6;
+
+console.log(n1 == n2); // True
+console.log(n1 != n3); // True
+console.log(n1 > n3)
+console.log(n1 < n2);
+

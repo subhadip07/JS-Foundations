8397c14ef5baaeac3185cc1e81aa85d7a5b9a173
subhadip07 <subhadipbag18@outlook.com>
2025-09-22 21:41:02 +0530

    add: logicals.js

---
+++ Part-1/logicals.js
@@ -0,0 +1,13 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedin = true;
+let ispaid = false;
+
+console.log(isLoggedin && ispaid);
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser);

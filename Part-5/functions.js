0609a0877934a45207291246f845399e699bdb1e
subhadip07 <subhadipbag18@outlook.com>
2025-09-27 13:35:00 +0530

    update: functions.js

--- Part-5/functions.js
+++ Part-5/functions.js
@@ -37,3 +37,37 @@ const calculateTotal = (price, quantity) => price * quantity;
 
 let totalCost = calculateTotal(499, 100);
 console.log(totalCost);
+
+/*
+4. Write a function named 'ProcessTeaOrder' that takes another function, 'makeTea', as a parameter and calls it with the argument '"earl grey"'.
+    Return the result of calling 'makeTea'.
+*/
+
+function makeTea(typeOfTea)
+{
+    return `maketea: ${typeOfTea}`;
+}
+
+function ProcessTeaOrder(teaFunction)
+{
+    return teaFunction("earl grey");
+}
+
+let order = ProcessTeaOrder(makeTea);
+console.log(order);
+
+/*
+5. Write a function named 'createTeaMaker' that returns another function. The returned function should take one parameter, 'teaType', and return a message like '"Making green tea"'.
+    Store the returned function in a variable named 'teaMaker' and call it with '"green tea"'.
+*/
+
+function createTeaMaker()
+{
+    return function (teaType)
+    {
+        return `Making ${teaType}`;
+    };
+}
+
+let teaMaker = createTeaMaker();
+console.log(teaMaker("Green tea"));
